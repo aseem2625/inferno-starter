1817887e7e09009b22c4f1e51592ec86f9997c29
aseemgupta <aseemgupta2625@gmail.com>
2017-05-10 18:48:18 +0530

    Add support to inferno-component. Convert home to inferno component

--- src/views/containers/Home.js
+++ src/views/containers/Home.js
@@ -1,4 +1,5 @@
 import Inferno from 'inferno';
+import Component from 'inferno-component';
 
 import { connect } from 'inferno-redux'
 import { bindActionCreators } from 'redux';
@@ -7,50 +8,60 @@ import * as actions from '../../reducers/modules/counter';
 import { Link } from 'inferno-router';
 import Card from '../components/Card';
 
-const Home = function (props) {
-	return (
-		<div className="page page__home">
-			<button className="temp-btn" onClick={props.increment}>REDUX COUNTER BUTTON</button>
-			<span>Counter: {props.counter}</span>
-			<Card>
-				<h1>Home</h1>
-				<p>This is the home page.</p>
+class Home extends Component {
+	componentWillMount() {
+		this.props.increment(); // incrementing a count
+	}
 
-				<p>You should check out:</p>
-				<nav>
-					<Link to="/foo">Foo</Link>
-					<Link to="/foo/bar">Foo/Bar</Link>
-				</nav>
-			</Card>
+	componentDidMount() {
+		this.props.increment(); // incrementing a count
+	}
 
-			<Card>
-				<h2>Features:</h2>
-				<ul>
-					<li>Offline Caching (via `serviceWorker`)</li>
-					<li>SASS & Autoprefixer</li>
-					<li>Asset Versioning (aka "cache-busting")</li>
-					<li>ES2015 (ES6) and ES2016 (ES7) support</li>
-					<li>Webpack Bundle Analysis (via `webpack-dashboard`)</li>
-					<li>Hot Module Replacement (HMR) for all files</li>
-					<li><a href="https://github.com/GoogleChrome/lighthouse" target="_blank" rel="noopener">Lighthouse</a> approved (100/100)</li>
-				</ul>
-			</Card>
+	render() {
+		return (
+			<div className="page page__home">
+				<button className="temp-btn" onClick={this.props.increment}>REDUX COUNTER BUTTON</button>
+				<span>Counter: {this.props.counter}</span>
+				<Card>
+					<h1>Home</h1>
+					<p>This is the home page.</p>
 
-			<Card>
-				<h2>Dev Dashboard</h2>
-				<p>With <a href="https://github.com/FormidableLabs/webpack-dashboard" target="_blank" rel="noopener">`webpack-dashboard`</a>, it's much easier to see what's happening inside your bundles. In addition to de-cluttering your `webpack-dev-server` log, you can quickly make sense of your bundles' `import`s and sizes.</p>
+					<p>You should check out:</p>
+					<nav>
+						<Link to="/foo">Foo</Link>
+						<Link to="/foo/bar">Foo/Bar</Link>
+					</nav>
+				</Card>
 
-				<img src="/img/dev-dash.jpg" />
+				<Card>
+					<h2>Features:</h2>
+					<ul>
+						<li>Offline Caching (via `serviceWorker`)</li>
+						<li>SASS & Autoprefixer</li>
+						<li>Asset Versioning (aka "cache-busting")</li>
+						<li>ES2015 (ES6) and ES2016 (ES7) support</li>
+						<li>Webpack Bundle Analysis (via `webpack-dashboard`)</li>
+						<li>Hot Module Replacement (HMR) for all files</li>
+						<li><a href="https://github.com/GoogleChrome/lighthouse" target="_blank" rel="noopener">Lighthouse</a> approved (100/100)</li>
+					</ul>
+				</Card>
 
-				<p>The dashboard is meant to be interactive (scrollable). If you are having issues, please see the author's note:</p>
-				<p>
-					<blockquote>
-						<strong>OSX Terminal.app users:</strong> Make sure that <strong>View → Allow Mouse Reporting</strong> is enabled, otherwise scrolling through logs and modules won't work. If your version of Terminal.app doesn't have this feature, you may want to check out an alternative such as <a href="https://www.iterm2.com/index.html" target="_blank" rel="noopener">iTerm2</a>.
-					</blockquote>
-				</p>
-			</Card>
-		</div>
-	);
+				<Card>
+					<h2>Dev Dashboard</h2>
+					<p>With <a href="https://github.com/FormidableLabs/webpack-dashboard" target="_blank" rel="noopener">`webpack-dashboard`</a>, it's much easier to see what's happening inside your bundles. In addition to de-cluttering your `webpack-dev-server` log, you can quickly make sense of your bundles' `import`s and sizes.</p>
+
+					<img src="/img/dev-dash.jpg" />
+
+					<p>The dashboard is meant to be interactive (scrollable). If you are having issues, please see the author's note:</p>
+					<p>
+						<blockquote>
+							<strong>OSX Terminal.app users:</strong> Make sure that <strong>View → Allow Mouse Reporting</strong> is enabled, otherwise scrolling through logs and modules won't work. If your version of Terminal.app doesn't have this feature, you may want to check out an alternative such as <a href="https://www.iterm2.com/index.html" target="_blank" rel="noopener">iTerm2</a>.
+						</blockquote>
+					</p>
+				</Card>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
